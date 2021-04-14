09fce3dfa54ee80c8fea2c370026ce538ff82226
Scott Arbeitman <scott@cultureamp.com>
2021-04-14 18:54:59 +1000

    Use union query for listing relations. (#12)
    * Use UNION query for listing relations.
    * Drop ilike in adapters.sql

--- dbt/include/athena/macros/adapters.sql
+++ dbt/include/athena/macros/adapters.sql
@@ -1,8 +1,3 @@
-
-{% macro ilike(column, value) -%}
-	regexp_like({{ column }}, '(?i)\A{{ value }}\Z')
-{%- endmacro %}
-
 {% macro set_table_classification(relation, default_value) -%}
     {%- set format = config.get('format', default=default_value) -%}
 
@@ -63,27 +58,32 @@
 
 {% macro athena__list_relations_without_caching(schema_relation) %}
   {% call statement('list_relations_without_caching', fetch_result=True) -%}
-    select
-      tables.table_catalog as database,
-      tables.table_name as name,
-      tables.table_schema as schema,
+    WITH views AS (
+      select
+        table_catalog as database,
+        table_name as name,
+        table_schema as schema
+      from {{ schema_relation.information_schema() }}.views
+      where LOWER(table_schema) = LOWER('{{ schema_relation.schema }}')
+    ), tables AS (
+      select
+        table_catalog as database,
+        table_name as name,
+        table_schema as schema
 
-      case
-        when views.table_name is not null
-            then 'view'
-        when table_type = 'BASE TABLE'
-            then 'table'
-        else table_type
-      end as table_type
+      from {{ schema_relation.information_schema() }}.tables
+      where LOWER(table_schema) = LOWER('{{ schema_relation.schema }}')
 
-    from {{ schema_relation.information_schema() }}.tables
-    left join {{ schema_relation.information_schema() }}.views
-      on tables.table_catalog = views.table_catalog
-      and tables.table_schema = views.table_schema
-      and tables.table_name = views.table_name
-    where {{ ilike('tables.table_schema', schema_relation.schema) }}
+      -- Views appear in both `tables` and `views`, so excluding them from tables
+      EXCEPT 
+
+      select * from views
+    )
+    select views.*, 'view' AS table_type FROM views
+    UNION ALL
+    select tables.*, 'table' AS table_type FROM tables
   {% endcall %}
-  {{ return(load_result('list_relations_without_caching').table) }}
+  {% do return(load_result('list_relations_without_caching').table) %}
 {% endmacro %}
 
 {% macro athena__get_columns_in_relation(relation) -%}
@@ -97,16 +97,16 @@
           null as numeric_scale
 
       from {{ relation.information_schema('columns') }}
-      where {{ ilike('table_name', relation.identifier) }}
+      where LOWER(table_name) = LOWER('{{ relation.identifier }}')
         {% if relation.schema %}
-            and {{ ilike('table_schema', relation.schema) }}
+            and LOWER(table_schema) = LOWER('{{ relation.schema }}')
         {% endif %}
       order by ordinal_position
 
   {% endcall %}
 
   {% set table = load_result('get_columns_in_relation').table %}
-  {{ return(sql_convert_columns_in_relation(table)) }}
+  {% do return(sql_convert_columns_in_relation(table)) %}
 {% endmacro %}
 
 {% macro athena__drop_relation(relation) -%}
