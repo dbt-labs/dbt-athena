9baa80c40a833c958a7b56c1585157da965eb799
Daniël Heres <danielheres@gmail.com>
2021-11-24 14:36:32 +0100

    Avoid clean up of table wen incremental_strategy is not insert_overwrite (#43)

--- dbt/include/athena/macros/adapters.sql
+++ dbt/include/athena/macros/adapters.sql
@@ -110,7 +110,9 @@
 {% endmacro %}
 
 {% macro athena__drop_relation(relation) -%}
-  {%- do adapter.clean_up_table(relation.schema, relation.table) -%}
+  {% if config.get('incremental_strategy') == 'insert_overwrite' %}
+    {%- do adapter.clean_up_table(relation.schema, relation.table) -%}
+  {% endif %}
   {% call statement('drop_relation', auto_begin=False) -%}
     drop {{ relation.type }} if exists {{ relation }}
   {%- endcall %}
