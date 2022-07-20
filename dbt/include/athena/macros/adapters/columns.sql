d4ae58953574f1036c1ace5411fa95518e0a98ea
Hironori Yamamoto <hironori.yamamoto@gunosy.com>
2022-07-20 19:34:14 +0900

    supports `on_schema_change` option

--- dbt/include/athena/macros/adapters/columns.sql
+++ dbt/include/athena/macros/adapters/columns.sql
@@ -20,3 +20,41 @@
   {% set table = load_result('get_columns_in_relation').table %}
   {% do return(sql_convert_columns_in_relation(table)) %}
 {% endmacro %}
+
+{% macro alter_relation_add_columns(relation, add_columns = none) -%}
+  {% if add_columns is none %}
+    {% set add_columns = [] %}
+  {% endif %}
+
+  {% set sql -%}
+      alter {{ relation.type }} {{ relation }}
+          add columns (
+            {%- for column in add_columns -%}
+                {{ column.name }} {{ column.data_type }}{{ ', ' if not loop.last }}
+            {%- endfor -%}
+          )
+  {%- endset -%}
+
+  {% if (add_columns | length) > 0 %}
+    {{ return(run_query(sql)) }}
+  {% endif %}
+{% endmacro %}
+
+{% macro alter_relation_replace_columns(relation, replace_columns = none) -%}
+  {% if replace_columns is none %}
+    {% set replace_columns = [] %}
+  {% endif %}
+
+  {% set sql -%}
+      alter {{ relation.type }} {{ relation }}
+          replace columns (
+            {%- for column in replace_columns -%}
+                {{ column.name }} {{ column.data_type }}{{ ', ' if not loop.last }}
+            {%- endfor -%}
+          )
+  {%- endset -%}
+
+  {% if (replace_columns | length) > 0 %}
+    {{ return(run_query(sql)) }}
+  {% endif %}
+{% endmacro %}
