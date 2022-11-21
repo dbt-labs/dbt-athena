ee0e69c015a35d5e8e01608acc5f1db1d3597119
Hironori Yamamoto <hironori.yamamoto@gunosy.com>
2022-11-21 10:49:52 +0900

    overwrite _QueryComment on package root

--- dbt/adapters/athena/__init__.py
+++ dbt/adapters/athena/__init__.py
@@ -1,14 +1,16 @@
-from dbt.adapters.base import AdapterPlugin
+from dbt.adapters.base import AdapterPlugin, query_headers
 
-from dbt.adapters.athena.column import AthenaColumn
 from dbt.adapters.athena.connections import AthenaConnectionManager, AthenaCredentials
 from dbt.adapters.athena.impl import AthenaAdapter
+from dbt.adapters.athena.query_headers import _QueryComment
 from dbt.include import athena
 
 Plugin = AdapterPlugin(adapter=AthenaAdapter, credentials=AthenaCredentials, include_path=athena.PACKAGE_PATH)
 
+# overwrite _QueryComment to add leading "-- " to query_comment
+query_headers._QueryComment = _QueryComment
+
 __all__ = [
-    "AthenaColumn",
     "AthenaConnectionManager",
     "AthenaCredentials",
     "AthenaAdapter",
