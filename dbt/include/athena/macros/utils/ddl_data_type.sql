cb573a4036d2f951f85304147510f67b159402bb
Hironori Yamamoto <hironori.yamamoto@gunosy.com>
2022-11-22 00:59:55 +0900

    fix transformation of string

--- dbt/include/athena/macros/utils/ddl_data_type.sql
+++ dbt/include/athena/macros/utils/ddl_data_type.sql
@@ -3,13 +3,7 @@
 {% macro ddl_data_type(col_type) -%}
     -- transform varchar
   {% set re = modules.re %}
-  {% set ns = namespace(ddl_type=col_type) %}
-  {% set matching_strings = re.findall('(varchar\(\d+\)|character varying\(\d+\))', ns.ddl_type) %}
-  {% for varchar_part in matching_strings %}
-    {% set ns.ddl_type = ns.ddl_type.replace(varchar_part, 'string') %}
-  {% endfor %}
-
-  {% set data_type = ns.ddl_type -%}
+  {% set data_type = re.sub('(?:varchar|character varying)(?:\(\d+\))?', 'string', col_type) %}
 
   -- transform array and map
   {%- if 'array' in data_type or 'map' in data_type -%}
