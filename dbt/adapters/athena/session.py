827dab9e6afb031f92a033caedef81ba937ad0fe
Jesse Dobbelaere <jesse@dobbelae.re>
2022-11-22 11:29:25 +0100

    Cleanup connection None

--- dbt/adapters/athena/session.py
+++ dbt/adapters/athena/session.py
@@ -2,10 +2,7 @@ import boto3.session
 from dbt.contracts.connection import Connection
 
 
-def get_boto3_session(connection: Connection = None) -> boto3.session.Session:
-    if connection is None:
-        raise RuntimeError("A Connection object needs to be passed to initialize the boto3 session")
-
+def get_boto3_session(connection: Connection) -> boto3.session.Session:
     return boto3.session.Session(
         region_name=connection.credentials.region_name,
         profile_name=connection.credentials.aws_profile_name,
