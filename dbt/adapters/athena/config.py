af2dd5977af9b0b193dd98d176f06c5424dccd3d
Armando Segnini <segnina@amazon.com>
2022-11-23 13:30:09 +0000

    Adding cache to reduce execution time

--- dbt/adapters/athena/config.py
+++ dbt/adapters/athena/config.py
@@ -1,7 +1,10 @@
+from functools import lru_cache
+
 import pkg_resources
 from botocore import config
 
 
+@lru_cache()
 def get_boto3_config() -> config.Config:
     return config.Config(
         user_agent_extra="dbt-athena-community/" + pkg_resources.get_distribution("dbt-athena-community").version
