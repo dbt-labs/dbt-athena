54b2601aadb6ebe28c96913bd0372e37415d4a37
nicor88 <6278547+nicor88@users.noreply.github.com>
2022-11-25 09:29:39 +0100

    Add version in the adapter (#59)
    * add version
    
    * add version

--- setup.py
+++ setup.py
@@ -1,17 +1,37 @@
 #!/usr/bin/env python
 import os
+import re
 
 from setuptools import find_namespace_packages, setup
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
+
 with open(os.path.join(this_directory, "README.md")) as f:
     long_description = f.read()
 
-
 package_name = "dbt-athena-community"
 
+
+# get version from a separate file
+def _get_plugin_version_dict():
+    _version_path = os.path.join(this_directory, "dbt", "adapters", "athena", "__version__.py")
+    _semver = r"""(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)"""
+    _pre = r"""((?P<prekind>a|b|rc)(?P<pre>\d+))?"""
+    _version_pattern = rf"""version\s*=\s*["']{_semver}{_pre}["']"""
+    with open(_version_path) as f:
+        match = re.search(_version_pattern, f.read().strip())
+        if match is None:
+            raise ValueError(f"invalid version at {_version_path}")
+        return match.groupdict()
+
+
+def _get_package_version():
+    parts = _get_plugin_version_dict()
+    return f'{parts["major"]}.{parts["minor"]}.{parts["patch"]}'
+
+
 dbt_version = "1.3"
-package_version = "1.3.2"
+package_version = _get_package_version()
 description = "The athena adapter plugin for dbt (data build tool)"
 
 if not package_version.startswith(dbt_version):
