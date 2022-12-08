b9d3d16141abf0ff90c9e72312b4c432879ae2f1
Jérémy Guiselin <9251353+Jrmyy@users.noreply.github.com>
2022-12-08 11:10:06 +0100

    feat!: Iceberg features only compatible with v3 engine (#79)
    * feat!: Iceberg features only compatible with v3 engine
    Closes #72
    
    * feat: add table properties in ctas
    
    * chore: update README

--- dbt/include/athena/macros/adapters/relation.sql
+++ dbt/include/athena/macros/adapters/relation.sql
@@ -1,8 +1,15 @@
-{% macro athena__drop_relation(relation) -%}
-  {% if config.get('incremental_strategy') != 'append' %}
+{% macro drop_relation(relation) -%}
+  {% if config.get('format') != 'iceberg' and config.get('incremental_strategy') != 'append' %}
     {%- do adapter.clean_up_table(relation.schema, relation.table) -%}
   {% endif %}
   {% call statement('drop_relation', auto_begin=False) -%}
     drop {{ relation.type }} if exists {{ relation }}
   {%- endcall %}
 {% endmacro %}
+
+{% macro set_table_classification(relation) -%}
+  {%- set format = config.get('format') -%}
+  {% call statement('set_table_classification', auto_begin=False) -%}
+    alter table {{ relation }} set tblproperties ('classification' = '{{ format }}')
+  {%- endcall %}
+{%- endmacro %}
