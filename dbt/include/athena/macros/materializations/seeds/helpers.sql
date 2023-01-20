6fe22b1f5baec3fcab33d69d8a93f0a61392f6e3
Andy Tr <andy.truong@choco.com>
2023-01-20 09:51:14 +0100

    feat: add support for external_location for seeds (#86)
    Co-authored-by: Mattia <5013654+mattiamatrix@users.noreply.github.com>
    Co-authored-by: Jérémy Guiselin <9251353+Jrmyy@users.noreply.github.com>
    Co-authored-by: nicor88 <6278547+nicor88@users.noreply.github.com>

--- dbt/include/athena/macros/materializations/seeds/helpers.sql
+++ dbt/include/athena/macros/materializations/seeds/helpers.sql
@@ -8,10 +8,11 @@
 {% endmacro %}
 
 {% macro athena__create_csv_table(model, agate_table) %}
-  {%- set column_override = model['config'].get('column_types', {}) -%}
-  {%- set quote_seed_column = model['config'].get('quote_columns', None) -%}
+  {%- set column_override = config.get('column_types', {}) -%}
+  {%- set quote_seed_column = config.get('quote_columns', None) -%}
   {%- set s3_data_dir = config.get('s3_data_dir', default=target.s3_data_dir) -%}
-  {%- set s3_data_naming = model['config'].get('s3_data_naming', target.s3_data_naming) -%}
+  {%- set s3_data_naming = config.get('s3_data_naming', target.s3_data_naming) -%}
+  {%- set external_location = config.get('external_location', default=none) -%}
 
   {% set sql %}
     create external table {{ this.render() }} (
@@ -23,7 +24,7 @@
         {%- endfor -%}
     )
     stored as parquet
-    location '{{ adapter.s3_table_location(s3_data_dir, s3_data_naming, model["schema"], model["alias"]) }}'
+    location '{{ adapter.s3_table_location(s3_data_dir, s3_data_naming, model["schema"], model["alias"], external_location) }}'
     tblproperties ('classification'='parquet')
   {% endset %}
 
