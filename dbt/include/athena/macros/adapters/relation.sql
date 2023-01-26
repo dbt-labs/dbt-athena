7eab3a28594e3fd6735ef96898ea4fdbe5392f4b
Jurgis Pods <10235147+jurgispods@users.noreply.github.com>
2023-01-26 13:47:55 +0100

    fix: prevent errors in drop_table macro when table doesn'tt exist

--- dbt/include/athena/macros/adapters/relation.sql
+++ dbt/include/athena/macros/adapters/relation.sql
@@ -1,11 +1,13 @@
 {% macro athena__drop_relation(relation) -%}
   {% set rel_type = adapter.get_table_type(relation.schema, relation.table) %}
-  {%- if rel_type is not none and rel_type == 'table' %}
-    {%- do adapter.clean_up_table(relation.schema, relation.table) -%}
-  {%- endif %}
+  {%- if rel_type is not none %}
+    {%- if rel_type == 'table' %}
+      {%- do adapter.clean_up_table(relation.schema, relation.table) -%}
+    {%- endif %}
     {% call statement('drop_relation', auto_begin=False) -%}
-    drop {{ relation.type }} if exists {{ relation }}
-  {%- endcall %}
+      drop {{ relation.type }} if exists {{ relation }}
+    {%- endcall %}
+  {%- endif %}
 {% endmacro %}
 
 {% macro set_table_classification(relation) -%}
