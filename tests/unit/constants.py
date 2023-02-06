8bb1cda18be346c6fe9b58030ab3db822a8dfcac
nicor88 <6278547+nicor88@users.noreply.github.com>
2023-02-06 12:50:26 +0100

    add unit tests

--- tests/unit/constants.py
+++ tests/unit/constants.py
@@ -1,5 +1,5 @@
 CATALOG_ID = "catalog_id"
 DATA_CATALOG_NAME = "awsdatacatalog"
 DATABASE_NAME = "test_dbt_athena"
-BUCKET = "test-dbt-athena-test-delete-partitions"
+BUCKET = "test-dbt-athena"
 AWS_REGION = "eu-west-1"
