dd7a3589a64999cde54513ef1186f11f8a448910
bbates4046 <30411048+bbates4046@users.noreply.github.com>
2023-02-20 16:22:22 -0700

    athena snapshot sql iceberg compatible

--- dbt/include/athena/macros/materializations/snapshots/snapshot.sql
+++ dbt/include/athena/macros/materializations/snapshots/snapshot.sql
@@ -12,36 +12,50 @@
 {%- endmacro %}
 
 {%
-  Recreate the snapshot table from the new_snapshot_table
+  Update the standard snapshot merge to include the DBT_INTERNAL_SOURCE prefix in the src_cols_csv
 %}
 
-{% macro athena__snapshot_merge_sql(target, source) -%}
-    {%- set target_relation = adapter.get_relation(database=target.database, schema=target.schema, identifier=target.identifier) -%}
-    {%- if target_relation is not none -%}
-      {% do adapter.drop_relation(target_relation) %}
-    {%- endif -%}
+{% macro athena__snapshot_merge_sql(target, source, insert_cols) -%}
+    {%- set insert_cols_csv = insert_cols | join(', ') -%}
+    {%- set src_columns = [] -%}
+    {%- for col in insert_cols -%}
+      {%- do src_columns.append('DBT_INTERNAL_SOURCE.' + col) -%}
+    {%- endfor -%}
+    {%- set src_cols_csv = src_columns | join(', ') -%}
 
-    {% set sql -%}
-      SELECT * FROM {{ source }};
-    {%- endset -%}
+    merge into {{ target }} as DBT_INTERNAL_DEST
+    using {{ source }} as DBT_INTERNAL_SOURCE
+    on DBT_INTERNAL_SOURCE.dbt_scd_id = DBT_INTERNAL_DEST.dbt_scd_id
 
-    {{ create_table_as(False, target_relation, sql) }}
+    when matched
+     and DBT_INTERNAL_DEST.dbt_valid_to is null
+     and DBT_INTERNAL_SOURCE.dbt_change_type in ('update', 'delete')
+        then update
+        set dbt_valid_to = DBT_INTERNAL_SOURCE.dbt_valid_to
+
+    when not matched
+     and DBT_INTERNAL_SOURCE.dbt_change_type = 'insert'
+        then insert ({{ insert_cols_csv }})
+        values ({{ src_cols_csv }})
 {% endmacro %}
+
+{%- macro snapshot_get_time() -%}
+    CAST(CURRENT_TIMESTAMP AS TIMESTAMP(6))
+{%- endmacro -%}
 
 {%
   Create the snapshot table from the source with dbt snapshot columns
 %}
 
-{% macro build_snapshot_table(strategy, source_sql) %}
-    SELECT *
-      , {{ strategy.unique_key }} AS dbt_unique_key
-      , {{ strategy.updated_at }} AS dbt_valid_from
-      , {{ strategy.scd_id }} AS dbt_scd_id
-      , 'insert' AS dbt_change_type
-      , CAST('9999-01-01' as timestamp) AS dbt_valid_to
-      , True AS is_current_record
-      , {{ current_timestamp() }} AS dbt_snapshot_at
-    FROM ({{ source_sql }}) source;
+{% macro build_snapshot_table(strategy, sql) %}
+    select *,
+        {{ strategy.scd_id }} as dbt_scd_id,
+        {{ strategy.updated_at }} as dbt_updated_at,
+        {{ strategy.updated_at }} as dbt_valid_from,
+        nullif({{ strategy.updated_at }}, {{ strategy.updated_at }}) as dbt_valid_to
+    from (
+        {{ sql }}
+    ) sbq
 {% endmacro %}
 
 {%
@@ -49,77 +63,116 @@
 %}
 
 {% macro snapshot_staging_table(strategy, source_sql, target_relation) -%}
-    WITH snapshot_query AS (
+    with snapshot_query as (
+
         {{ source_sql }}
-    )
-    , snapshotted_data_base AS (
-        SELECT *
-          , ROW_NUMBER() OVER (
-              PARTITION BY dbt_unique_key
-              ORDER BY dbt_valid_from DESC
-            ) AS dbt_snapshot_rn
-        FROM {{ target_relation }}
-    )
-    , snapshotted_data AS (
-        SELECT *
-        FROM snapshotted_data_base
-        WHERE dbt_snapshot_rn = 1
-          AND dbt_change_type != 'delete'
-    )
-    , source_data AS (
-        SELECT *
-          , {{ strategy.unique_key }} AS dbt_unique_key
-          , {{ strategy.updated_at }} AS dbt_valid_from
-          , {{ strategy.scd_id }} AS dbt_scd_id
-        FROM snapshot_query
-    )
-    , upserts AS (
-        SELECT source_data.*
-          , CASE
-              WHEN snapshotted_data.dbt_unique_key IS NULL THEN 'insert'
-              ELSE 'update'
-            END as dbt_change_type
-          , CAST('9999-01-01' as timestamp) AS dbt_valid_to
-          , True AS is_current_record
-        FROM source_data
-        LEFT JOIN snapshotted_data
-               ON snapshotted_data.dbt_unique_key = source_data.dbt_unique_key
-        WHERE snapshotted_data.dbt_unique_key IS NULL
-           OR (
-                snapshotted_data.dbt_unique_key IS NOT NULL
-            AND (
+
+    ),
+
+    snapshotted_data as (
+
+        select *,
+            {{ strategy.unique_key }} as dbt_unique_key
+
+        from {{ target_relation }}
+        where dbt_valid_to is null
+
+    ),
+
+    insertions_source_data as (
+
+        select
+            *,
+            {{ strategy.unique_key }} as dbt_unique_key,
+            {{ strategy.updated_at }} as dbt_updated_at,
+            {{ strategy.updated_at }} as dbt_valid_from,
+            nullif({{ strategy.updated_at }}, {{ strategy.updated_at }}) as dbt_valid_to,
+            {{ strategy.scd_id }} as dbt_scd_id
+
+        from snapshot_query
+    ),
+
+    updates_source_data as (
+
+        select
+            *,
+            {{ strategy.unique_key }} as dbt_unique_key,
+            {{ strategy.updated_at }} as dbt_updated_at,
+            {{ strategy.updated_at }} as dbt_valid_from,
+            {{ strategy.updated_at }} as dbt_valid_to
+
+        from snapshot_query
+    ),
+
+    {%- if strategy.invalidate_hard_deletes %}
+
+    deletes_source_data as (
+
+        select
+            *,
+            {{ strategy.unique_key }} as dbt_unique_key
+        from snapshot_query
+    ),
+    {% endif %}
+
+    insertions as (
+
+        select
+            'insert' as dbt_change_type,
+            source_data.*
+
+        from insertions_source_data as source_data
+        left outer join snapshotted_data on snapshotted_data.dbt_unique_key = source_data.dbt_unique_key
+        where snapshotted_data.dbt_unique_key is null
+           or (
+                snapshotted_data.dbt_unique_key is not null
+            and (
                 {{ strategy.row_changed }}
             )
         )
+
+    ),
+
+    updates as (
+
+        select
+            'update' as dbt_change_type,
+            source_data.*,
+            snapshotted_data.dbt_scd_id
+
+        from updates_source_data as source_data
+        join snapshotted_data on snapshotted_data.dbt_unique_key = source_data.dbt_unique_key
+        where (
+            {{ strategy.row_changed }}
+        )
     )
+
     {%- if strategy.invalidate_hard_deletes -%}
-    {%- set target_columns = adapter.get_columns_in_relation(target_relation) -%}
-    , deletes AS (
-        SELECT
-        {% for column in target_columns if not column.name == 'dbt_snapshot_at' %}
-          {% if column.name == 'dbt_valid_from' %}
-            {{ current_timestamp() }} AS dbt_valid_from {%- if not loop.last -%},{%- endif -%}
-          {% elif column.name == 'dbt_change_type' %}
-            'delete' AS dbt_change_type {%- if not loop.last -%},{%- endif -%}
-          {% elif column.name == 'dbt_valid_to' %}
-            CAST('9999-01-01' as timestamp) AS dbt_valid_to {%- if not loop.last -%},{%- endif -%}
-          {% elif column.name == 'is_current_record' %}
-            True AS is_current_record {%- if not loop.last -%},{%- endif -%}
-          {% else %}
-            snapshotted_data.{{ column.name }} {%- if not loop.last -%},{%- endif -%}
-          {% endif %}
-        {% endfor %}
-        FROM snapshotted_data
-        LEFT JOIN source_data
-               ON snapshotted_data.dbt_unique_key = source_data.dbt_unique_key
-        WHERE source_data.dbt_unique_key IS NULL
+    ,
+
+    deletes as (
+
+        select
+            'delete' as dbt_change_type,
+            source_data.*,
+            {{ snapshot_get_time() }} as dbt_valid_from,
+            {{ snapshot_get_time() }} as dbt_updated_at,
+            {{ snapshot_get_time() }} as dbt_valid_to,
+            snapshotted_data.dbt_scd_id
+
+        from snapshotted_data
+        left join deletes_source_data as source_data on snapshotted_data.dbt_unique_key = source_data.dbt_unique_key
+        where source_data.dbt_unique_key is null
     )
-    SELECT * FROM upserts
-    UNION ALL
-    SELECT * FROM deletes;
-    {% else %}
-    SELECT * FROM upserts;
-    {% endif %}
+    {%- endif %}
+
+    select * from insertions
+    union all
+    select * from updates
+    {%- if strategy.invalidate_hard_deletes %}
+    union all
+    select * from deletes
+    {%- endif %}
 
 {%- endmacro %}
 
@@ -128,22 +181,15 @@
 %}
 
 {% macro athena__build_snapshot_staging_table(strategy, sql, target_relation) %}
-    {%- set tmp_identifier = target_relation.identifier ~ '__dbt_tmp' -%}
+    {% set temp_relation = make_temp_relation(target_relation) %}
 
-    {%- set tmp_relation = api.Relation.create(identifier=tmp_identifier,
-                                                  schema=target_relation.schema,
-                                                  database=target_relation.database,
-                                                  type='table') -%}
-
-    {% do adapter.drop_relation(tmp_relation) %}
-
-    {%- set select = snapshot_staging_table(strategy, sql, target_relation) -%}
+    {% set select = snapshot_staging_table(strategy, sql, target_relation) %}
 
     {% call statement('build_snapshot_staging_relation') %}
-        {{ create_table_as(False, tmp_relation, select) }}
+        {{ create_table_as(True, temp_relation, select) }}
     {% endcall %}
 
-    {% do return(tmp_relation) %}
+    {% do return(temp_relation) %}
 {% endmacro %}
 
 {#
@@ -175,69 +221,7 @@
   {%- endfor %}
 {% endmacro %}
 
-{#
-    Update the dbt_valid_to and is_current_record for
-    snapshot rows being updated and create a new temporary table to hold them
-#}
 
-{% macro athena__create_new_snapshot_table(target, source) %}
-    {%- set tmp_identifier = target.identifier ~ '__dbt_tmp_1' -%}
-
-    {%- set tmp_relation = adapter.get_relation(database=target.database, schema=target.schema, identifier=tmp_identifier) -%}
-
-    {%- set target_relation = api.Relation.create(identifier=tmp_identifier,
-      schema=target.schema,
-      database=none,
-      type='table') -%}
-
-    {%- set source_columns = adapter.get_columns_in_relation(source) -%}
-
-    {% if tmp_relation is not none %}
-      {% do adapter.drop_relation(tmp_relation) %}
-    {% endif %}
-
-    {% set sql -%}
-      SELECT
-        {% for column in source_columns %}
-          {{ column.name }} {%- if not loop.last -%},{%- endif -%}
-        {% endfor %}
-        ,dbt_snapshot_at
-      from {{ target }}
-      WHERE dbt_unique_key NOT IN ( SELECT dbt_unique_key FROM {{ source }} )
-      UNION ALL
-      SELECT
-        {% for column in source_columns %}
-              {% if column.name == 'dbt_valid_to' %}
-              CASE
-              WHEN dbt_valid_to=CAST('9999-01-01' as timestamp) AND is_current_record=True
-              THEN {{ current_timestamp() }}
-              ELSE dbt_valid_to
-              END AS dbt_valid_to {%- if not loop.last -%},{%- endif -%}
-              {% elif column.name == 'is_current_record' %}
-              CASE WHEN is_current_record=True THEN False ELSE is_current_record END
-              AS is_current_record {%- if not loop.last -%},{%- endif -%}
-              {% else %}
-                {{ column.name }} {%- if not loop.last -%},{%- endif -%}
-              {% endif %}
-        {% endfor %}
-        ,{{ current_timestamp() }} AS dbt_snapshot_at
-      from {{ target }}
-      WHERE dbt_unique_key IN ( SELECT dbt_unique_key FROM {{ source }} )
-      UNION ALL
-      SELECT
-        {% for column in source_columns %}
-            {{ column.name }} {%- if not loop.last -%},{%- endif -%}
-        {% endfor %}
-        ,{{ current_timestamp() }} AS dbt_snapshot_at
-      FROM {{ source }};
-    {%- endset -%}
-
-    {% call statement('create_new_snapshot_table') %}
-        {{ create_table_as(False, target_relation, sql) }}
-    {% endcall %}
-
-    {% do return(target_relation) %}
-{% endmacro %}
 
 {% materialization snapshot, adapter='athena' %}
   {%- set config = model['config'] -%}
@@ -245,19 +229,30 @@
   {%- set target_table = model.get('alias', model.get('name')) -%}
 
   {%- set strategy_name = config.get('strategy') -%}
-  {%- set unique_key = config.get('unique_key') %}
+  {%- set unique_key = config.get('unique_key') -%}
   {%- set file_format = config.get('file_format', 'parquet') -%}
+  {%- set table_type = config.get('table_type', 'iceberg') -%}
 
-  {{ log('Checking if target table exists') }}
+  {% if table_type != 'iceberg' %}
+      {{ exceptions.raise_not_implemented(
+    'snapshot materialization not implemented for any table type except iceberg')
+    }}
+  {% endif %}
+
   {% set target_relation_exists, target_relation = get_or_create_relation(
-          database=model.database,
+          database=none,
           schema=model.schema,
           identifier=target_table,
           type='table') -%}
 
+
   {% if not adapter.check_schema_exists(model.database, model.schema) %}
     {% do create_schema(model.database, model.schema) %}
   {% endif %}
+
+  {%- if not target_relation.is_table -%}
+    {% do exceptions.relation_wrong_type(target_relation, 'table') %}
+  {%- endif -%}
 
   {{ run_hooks(pre_hooks, inside_transaction=False) }}
 
@@ -275,15 +270,31 @@
 
       {% set staging_table = athena__build_snapshot_staging_table(strategy, sql, target_relation) %}
 
-      {% set missing_columns = adapter.get_missing_columns(staging_table, target_relation) %}
+      {% set missing_columns = adapter.get_missing_columns(staging_table, target_relation)
+                                   | rejectattr('name', 'equalto', 'dbt_change_type')
+                                   | rejectattr('name', 'equalto', 'DBT_CHANGE_TYPE')
+                                   | rejectattr('name', 'equalto', 'dbt_unique_key')
+                                   | rejectattr('name', 'equalto', 'DBT_UNIQUE_KEY')
+                                   | list %}
 
       {% do create_columns(target_relation, missing_columns) %}
 
-      {% set new_snapshot_table = athena__create_new_snapshot_table(target = target_relation, source = staging_table) %}
+      {% set source_columns = adapter.get_columns_in_relation(staging_table)
+                                   | rejectattr('name', 'equalto', 'dbt_change_type')
+                                   | rejectattr('name', 'equalto', 'DBT_CHANGE_TYPE')
+                                   | rejectattr('name', 'equalto', 'dbt_unique_key')
+                                   | rejectattr('name', 'equalto', 'DBT_UNIQUE_KEY')
+                                   | list %}
+
+      {% set quoted_source_columns = [] %}
+      {% for column in source_columns %}
+        {% do quoted_source_columns.append(adapter.quote(column.name)) %}
+      {% endfor %}
 
       {% set final_sql = athena__snapshot_merge_sql(
             target = target_relation,
-            source = new_snapshot_table
+            source = staging_table,
+            insert_cols = quoted_source_columns
          )
       %}
 
@@ -301,9 +312,6 @@
       {% do adapter.drop_relation(staging_table) %}
   {% endif %}
 
-  {% if new_snapshot_table is defined %}
-      {% do adapter.drop_relation(new_snapshot_table) %}
-  {% endif %}
 
   {{ run_hooks(post_hooks, inside_transaction=False) }}
 
