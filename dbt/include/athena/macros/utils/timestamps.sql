c34254d414241dddffd7d919927ff95d39ce64d0
bbates4046 <30411048+bbates4046@users.noreply.github.com>
2023-03-06 16:27:22 -0600

    Update timestamps.sql

--- dbt/include/athena/macros/utils/timestamps.sql
+++ dbt/include/athena/macros/utils/timestamps.sql
@@ -1,5 +1,20 @@
+{%
+    pyathena converts time zoned timestamps to strings so lets avoid them now()
+    Iceberg tables and Athena V3 will require a local overwrite of the athena__current_timestamp macro to timestamp(6))
+%}
+
 {% macro athena__current_timestamp() -%}
-    -- pyathena converts time zoned timestamps to strings so lets avoid them
-    -- now()
     cast(now() as timestamp)
+{%- endmacro %}
+
+{%
+  Macro to get the end_of_time timestamp
+%}
+
+{% macro end_of_time() -%}
+  {{ return(adapter.dispatch('end_of_time')()) }}
+{%- endmacro %}
+
+{% macro athena__end_of_time() -%}
+  cast('9999-01-01' AS timestamp)
 {%- endmacro %}
