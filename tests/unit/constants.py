30792f4ed5bd2c3cbcf97015c7dc51d33706e09f
Serhii Dimchenko <39801237+svdimchenko@users.noreply.github.com>
2023-05-02 18:24:18 +0200

    fix: get data catalog method (#265)

--- tests/unit/constants.py
+++ tests/unit/constants.py
@@ -1,6 +1,6 @@
-CATALOG_ID = "catalog_id"
+CATALOG_ID = "12345678910"
 DATA_CATALOG_NAME = "awsdatacatalog"
-SHARED_DATA_CATALOG_NAME = "12345678910"
+SHARED_DATA_CATALOG_NAME = "9876543210"
 DATABASE_NAME = "test_dbt_athena"
 BUCKET = "test-dbt-athena"
 AWS_REGION = "eu-west-1"
