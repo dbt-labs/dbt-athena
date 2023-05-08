8c430d0b87af9f06d021611ff98b44614b2e2665
Pavel Roslovets <p.v.roslovets@gmail.com>
2023-05-08 07:38:33 +0200

    fix: source freshness (cast_timestamp for models without config field) (#287)

--- dbt/include/athena/macros/utils/timestamps.sql
+++ dbt/include/athena/macros/utils/timestamps.sql
@@ -8,7 +8,7 @@
 
 
 {% macro cast_timestamp(timestamp_col) -%}
-  {%- set config = model['config'] -%}
+  {%- set config = model.get('config', {}) -%}
   {%- set table_type = config.get('table_type', 'glue') -%}
   {%- if table_type == 'iceberg' -%}
     cast({{ timestamp_col }} as timestamp(6))
