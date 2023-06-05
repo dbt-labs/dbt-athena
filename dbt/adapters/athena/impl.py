3a5e1becb144c0510a020a34370f5667e19a8c5c
Julian Steger <julian.steger@scalable.capital>
2023-06-05 12:49:30 +0200

    batch_delete_partitions only supports 25 partitions

--- dbt/adapters/athena/impl.py
+++ dbt/adapters/athena/impl.py
@@ -522,7 +522,7 @@ class AthenaAdapter(SQLAdapter):
         # if source table has partitions we need to delete and add partitions
         # it source table hasn't any partitions we need to delete target table partitions
         if target_table_partitions:
-            for partition_batch in get_chunks(target_table_partitions, 100):
+            for partition_batch in get_chunks(target_table_partitions, 25):
                 glue_client.batch_delete_partition(
                     DatabaseName=target_relation.schema,
                     TableName=target_relation.identifier,
