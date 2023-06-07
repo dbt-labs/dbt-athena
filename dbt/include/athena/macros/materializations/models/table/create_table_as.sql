baa40be10935e1b4f0b8e01c42d315b1ab9fe496
Paul Brabban <paul.brabban@gmail.com>
2023-06-07 20:44:27 +0100

    use another adapter as reference for bool config param

--- dbt/include/athena/macros/materializations/models/table/create_table_as.sql
+++ dbt/include/athena/macros/materializations/models/table/create_table_as.sql
@@ -20,7 +20,7 @@
                                                  s3_data_naming,
                                                  external_location,
                                                  temporary) -%}
-  {%- set native_drop = config.get('native_drop', 'False') == 'True' -%}
+  {%- set native_drop = config.get('native_drop', default=false) -%}
 
   {%- set contract_config = config.get('contract') -%}
   {%- if contract_config.enforced -%}
