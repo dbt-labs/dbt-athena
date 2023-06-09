1fd436ef65547f6b8218c7ef1581e8f0efb9db51
Julian Steger <108534789+juliansteger-sc@users.noreply.github.com>
2023-06-09 13:40:28 +0200

    fix: BatchDeletePartitions only accepts up to 25 partitions (#328)

--- dbt/adapters/athena/utils.py
+++ dbt/adapters/athena/utils.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Collection, List, Optional, TypeVar
 
 from mypy_boto3_athena.type_defs import DataCatalogTypeDef
 
@@ -11,3 +11,12 @@ def clean_sql_comment(comment: str) -> str:
 def get_catalog_id(catalog: Optional[DataCatalogTypeDef]) -> Optional[str]:
     if catalog:
         return catalog["Parameters"]["catalog-id"]
+
+
+T = TypeVar("T")
+
+
+def get_chunks(lst: Collection[T], n: int) -> Collection[List[T]]:
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i : i + n]
