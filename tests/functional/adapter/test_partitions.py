a9a8696e08202f27aecd147b03c1187824529ed3
Lukas Gust <31327300+lukas-gust@users.noreply.github.com>
2023-09-06 01:25:52 -0600

    fix: Added null check for partitions batches (#392)
    Co-authored-by: nicor88 <6278547+nicor88@users.noreply.github.com>

--- tests/functional/adapter/test_partitions.py
+++ tests/functional/adapter/test_partitions.py
@@ -17,6 +17,67 @@ from (
 cross join unnest(date_array) as t2(date_column)
 """
 
+test_single_nullable_partition_model_sql = """
+with data as (
+    select
+        random() as col_1,
+        row_number() over() as id
+    from
+        unnest(sequence(1, 200))
+)
+
+select
+    col_1, id
+from data
+union all
+select random() as col_1, NULL as id
+union all
+select random() as col_1, NULL as id
+"""
+
+test_nullable_partitions_model_sql = """
+{{ config(
+    materialized='table',
+    format='parquet',
+    s3_data_naming='table',
+    partitioned_by=['id', 'date_column']
+) }}
+
+with data as (
+    select
+    random() as rnd,
+    row_number() over() as id,
+    cast(date_column as date) as date_column
+from (
+    values (
+        sequence(from_iso8601_date('2023-01-01'), from_iso8601_date('2023-07-31'), interval '1' day)
+    )
+) as t1(date_array)
+cross join unnest(date_array) as t2(date_column)
+)
+
+select
+    rnd,
+    case when id  <= 50 then null else id end as id,
+    date_column
+from data
+union all
+select
+    random() as rnd,
+    NULL as id,
+    NULL as date_column
+union all
+select
+    random() as rnd,
+    NULL as id,
+    cast('2023-09-02' as date) as date_column
+union all
+select
+    random() as rnd,
+    40 as id,
+    NULL as date_column
+"""
+
 
 class TestHiveTablePartitions:
     @pytest.fixture(scope="class")
@@ -125,3 +186,81 @@ class TestIcebergIncrementalPartitions:
         incremental_records_count = project.run_sql(model_run_result_row_count_query, fetch="all")[0][0]
 
         assert incremental_records_count == 212
+
+
+class TestHiveNullValuedPartitions:
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {
+            "models": {
+                "+table_type": "hive",
+                "+materialized": "table",
+                "+partitioned_by": ["id", "date_column"],
+            }
+        }
+
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "test_nullable_partitions_model.sql": test_nullable_partitions_model_sql,
+        }
+
+    def test__check_run_with_partitions(self, project):
+        relation_name = "test_nullable_partitions_model"
+        model_run_result_row_count_query = f"select count(*) as records from {project.test_schema}.{relation_name}"
+        model_run_result_null_id_count_query = (
+            f"select count(*) as records from {project.test_schema}.{relation_name} where id is null"
+        )
+        model_run_result_null_date_count_query = (
+            f"select count(*) as records from {project.test_schema}.{relation_name} where date_column is null"
+        )
+
+        first_model_run = run_dbt(["run", "--select", relation_name])
+        first_model_run_result = first_model_run.results[0]
+
+        # check that the model run successfully
+        assert first_model_run_result.status == RunStatus.Success
+
+        records_count_first_run = project.run_sql(model_run_result_row_count_query, fetch="all")[0][0]
+
+        assert records_count_first_run == 215
+
+        null_id_count_first_run = project.run_sql(model_run_result_null_id_count_query, fetch="all")[0][0]
+
+        assert null_id_count_first_run == 52
+
+        null_date_count_first_run = project.run_sql(model_run_result_null_date_count_query, fetch="all")[0][0]
+
+        assert null_date_count_first_run == 2
+
+
+class TestHiveSingleNullValuedPartition:
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {
+            "models": {
+                "+table_type": "hive",
+                "+materialized": "table",
+                "+partitioned_by": ["id"],
+            }
+        }
+
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "test_single_nullable_partition_model.sql": test_single_nullable_partition_model_sql,
+        }
+
+    def test__check_run_with_partitions(self, project):
+        relation_name = "test_single_nullable_partition_model"
+        model_run_result_row_count_query = f"select count(*) as records from {project.test_schema}.{relation_name}"
+
+        first_model_run = run_dbt(["run", "--select", relation_name])
+        first_model_run_result = first_model_run.results[0]
+
+        # check that the model run successfully
+        assert first_model_run_result.status == RunStatus.Success
+
+        records_count_first_run = project.run_sql(model_run_result_row_count_query, fetch="all")[0][0]
+
+        assert records_count_first_run == 202
