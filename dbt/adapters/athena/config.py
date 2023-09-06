5a9b8b3d541285c8f7253d07457c481b163c3237
Shunsuke Kirino <skirino@flywheel.jp>
2023-09-06 15:21:04 +0900

    fix: Use `importlib.metadata.version()` to remove implicit dependency on `setuptools`

--- dbt/adapters/athena/config.py
+++ dbt/adapters/athena/config.py
@@ -1,11 +1,9 @@
+import importlib.metadata
 from functools import lru_cache
 
-import pkg_resources
 from botocore import config
 
 
 @lru_cache()
 def get_boto3_config() -> config.Config:
-    return config.Config(
-        user_agent_extra="dbt-athena-community/" + pkg_resources.get_distribution("dbt-athena-community").version
-    )
+    return config.Config(user_agent_extra="dbt-athena-community/" + importlib.metadata.version("dbt-athena-community"))
--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -1,13 +1,12 @@
+import importlib.metadata
 from unittest.mock import Mock
-
-import pkg_resources
 
 from dbt.adapters.athena.config import get_boto3_config
 
 
 class TestConfig:
     def test_get_boto3_config(self):
-        pkg_resources.get_distribution = Mock(return_value=pkg_resources.Distribution(version="2.4.6"))
+        importlib.metadata.version = Mock(return_value="2.4.6")
         get_boto3_config.cache_clear()
         config = get_boto3_config()
         assert config._user_provided_options["user_agent_extra"] == "dbt-athena-community/2.4.6"
