5401574d7cf93d940da13c9c54565ced6fea72ff
Serhii Dimchenko <svdimchenko@gmail.com>
2023-09-25 16:52:22 +0200

    Minor fix

--- dbt/include/athena/macros/utils/timestamps.sql
+++ dbt/include/athena/macros/utils/timestamps.sql
@@ -10,7 +10,7 @@
 {% macro cast_timestamp(timestamp_col) -%}
   {%- set config = model.get('config', {}) -%}
   {%- set table_type = config.get('table_type', 'glue') -%}
-  {%- if table_type == 'iceberg' and not model.is_view -%}
+  {%- if table_type == 'iceberg' and not config.get('materialized', 'table') == 'view' -%}
     cast({{ timestamp_col }} as timestamp(6))
   {%- else -%}
     cast({{ timestamp_col }} as timestamp)
