cd4837a6ea0f3394210a2c5f3d2d94a93fc00694
Serhii Dimchenko <svdimchenko@gmail.com>
2023-09-25 16:47:37 +0200

    Fixed current_timestamp macro for views

--- dbt/include/athena/macros/utils/timestamps.sql
+++ dbt/include/athena/macros/utils/timestamps.sql
@@ -10,7 +10,7 @@
 {% macro cast_timestamp(timestamp_col) -%}
   {%- set config = model.get('config', {}) -%}
   {%- set table_type = config.get('table_type', 'glue') -%}
-  {%- if table_type == 'iceberg' -%}
+  {%- if table_type == 'iceberg' and not model.is_view -%}
     cast({{ timestamp_col }} as timestamp(6))
   {%- else -%}
     cast({{ timestamp_col }} as timestamp)
