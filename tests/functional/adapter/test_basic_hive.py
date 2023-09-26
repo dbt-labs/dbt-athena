71dbc98c1d56f7465a530a1a71b7f663d2d3dfe5
Serhii Dimchenko <svdimchenko@gmail.com>
2023-09-26 15:05:10 +0200

    Add debug for test

--- tests/functional/adapter/test_basic_hive.py
+++ tests/functional/adapter/test_basic_hive.py
@@ -15,6 +15,7 @@ from dbt.tests.adapter.basic.test_singular_tests_ephemeral import (
 )
 from dbt.tests.adapter.basic.test_snapshot_check_cols import BaseSnapshotCheckCols
 from dbt.tests.adapter.basic.test_snapshot_timestamp import BaseSnapshotTimestamp
+from dbt.tests.util import check_result_nodes_by_name, run_dbt
 
 
 class TestSimpleMaterializationsHive(BaseSimpleMaterializations):
@@ -26,7 +27,28 @@ class TestSingularTestsHive(BaseSingularTests):
 
 
 class TestSingularTestsEphemeralHive(BaseSingularTestsEphemeral):
-    pass
+    def test_singular_tests_ephemeral(self, project):
+        # check results from seed command
+        results = run_dbt(["seed"])
+        assert len(results) == 1
+        check_result_nodes_by_name(results, ["base"])
+
+        # Check results from test command
+        results = run_dbt(["--debug", "test"], expect_pass=False)
+        assert len(results) == 2
+        check_result_nodes_by_name(results, ["passing", "failing"])
+
+        # Check result status
+        for result in results:
+            if result.node.name == "passing":
+                assert result.status == "pass"
+            elif result.node.name == "failing":
+                assert result.status == "fail"
+
+        # check results from run command
+        results = run_dbt()
+        assert len(results) == 2
+        check_result_nodes_by_name(results, ["failing_model", "passing_model"])
 
 
 class TestEmptyHive(BaseEmpty):
