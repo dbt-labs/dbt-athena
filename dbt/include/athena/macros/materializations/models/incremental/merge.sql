412fb58670fd2aeefd8b3b9c65f60b43476de316
Serhii Dimchenko <svdimchenko@gmail.com>
2023-10-04 15:13:50 +0200

    Implement update condition for merge strategy

--- dbt/include/athena/macros/materializations/models/incremental/merge.sql
+++ dbt/include/athena/macros/materializations/models/incremental/merge.sql
@@ -47,7 +47,18 @@
     {{ "," if not is_last }}
 {%- endmacro -%}
 
-{% macro iceberg_merge(on_schema_change, tmp_relation, target_relation, unique_key, incremental_predicates, existing_relation, delete_condition, statement_name="main") %}
+{% macro iceberg_merge(
+    on_schema_change,
+    tmp_relation,
+    target_relation,
+    unique_key,
+    incremental_predicates,
+    existing_relation,
+    delete_condition,
+    update_condition,
+    statement_name="main"
+  )
+%}
     {%- set merge_update_columns = config.get('merge_update_columns') -%}
     {%- set merge_exclude_columns = config.get('merge_exclude_columns') -%}
     {%- set merge_update_columns_default_rule = config.get('merge_update_columns_default_rule', 'replace') -%}
@@ -98,6 +109,9 @@
       {%- endif %}
       {% if update_columns -%}
         when matched
+          {% if update_condition is not none -%}
+            and {{ update_condition }}
+          {%- endif %}
           then update set
             {%- for col in update_columns %}
               {%- if merge_update_columns_rules and col.name in merge_update_columns_rules %}
