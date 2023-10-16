cab91ff68e59a3319abf5a4b0876636e644ae1bc
Benjamin Dornel <62495124+benjamin-awd@users.noreply.github.com>
2023-10-16 15:03:17 +0800

    feat: allow aws session token in profiles file (#459)

--- dbt/adapters/athena/session.py
+++ dbt/adapters/athena/session.py
@@ -7,6 +7,7 @@ def get_boto3_session(connection: Connection) -> boto3.session.Session:
     return boto3.session.Session(
         aws_access_key_id=connection.credentials.aws_access_key_id,
         aws_secret_access_key=connection.credentials.aws_secret_access_key,
+        aws_session_token=connection.credentials.aws_session_token,
         region_name=connection.credentials.region_name,
         profile_name=connection.credentials.aws_profile_name,
     )
