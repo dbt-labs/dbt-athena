da65da579100576338f931e279e4ae5c0dd335c4
Roman Korsun <romakorsun2000@gmail.com>
2024-01-17 13:00:01 +0200

    Add lf-inherited-tags for view materialization

--- dbt/include/athena/macros/materializations/models/view/view.sql
+++ dbt/include/athena/macros/materializations/models/view/view.sql
@@ -1,6 +1,7 @@
 {% materialization view, adapter='athena' -%}
     {%- set identifier = model['alias'] -%}
     {%- set lf_tags_config = config.get('lf_tags_config') -%}
+    {%- set lf_inherited_tags = config.get('lf_inherited_tags') -%}
     {%- set lf_grants = config.get('lf_grants') -%}
     {%- set versions_to_keep = config.get('versions_to_keep', default=4) -%}
     {%- set target_relation = api.Relation.create(identifier=identifier,
@@ -15,7 +16,7 @@
     {% set target_relation = this.incorporate(type='view') %}
 
     {% if lf_tags_config is not none %}
-      {{ adapter.add_lf_tags(target_relation, lf_tags_config) }}
+      {{ adapter.add_lf_tags(target_relation, lf_tags_config, lf_inherited_tags) }}
     {% endif %}
 
     {% if lf_grants is not none %}
