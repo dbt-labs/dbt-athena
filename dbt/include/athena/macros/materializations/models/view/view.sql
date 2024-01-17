a3bbfd130b095af742a7bda5e0bdf523f6b1950f
Roman Korsun <romakorsun2000@gmail.com>
2024-01-17 13:19:51 +0200

    Check lf-tags before new target_relation

--- dbt/include/athena/macros/materializations/models/view/view.sql
+++ dbt/include/athena/macros/materializations/models/view/view.sql
@@ -1,8 +1,10 @@
 {% materialization view, adapter='athena' -%}
     {%- set identifier = model['alias'] -%}
+
     {%- set lf_tags_config = config.get('lf_tags_config') -%}
     {%- set lf_inherited_tags = config.get('lf_inherited_tags') -%}
     {%- set lf_grants = config.get('lf_grants') -%}
+
     {%- set versions_to_keep = config.get('versions_to_keep', default=4) -%}
     {%- set target_relation = api.Relation.create(identifier=identifier,
                                                 schema=schema,
@@ -13,8 +15,6 @@
 
     {% do adapter.expire_glue_table_versions(target_relation, versions_to_keep, False) %}
 
-    {% set target_relation = this.incorporate(type='view') %}
-
     {% if lf_tags_config is not none %}
       {{ adapter.add_lf_tags(target_relation, lf_tags_config, lf_inherited_tags) }}
     {% endif %}
@@ -22,6 +22,8 @@
     {% if lf_grants is not none %}
       {{ adapter.apply_lf_grants(target_relation, lf_grants) }}
     {% endif %}
+
+    {% set target_relation = this.incorporate(type='view') %}
 
     {% do persist_docs(target_relation, model) %}
 
