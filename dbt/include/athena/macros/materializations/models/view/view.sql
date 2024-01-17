ddfb405e3765545b1b6fa8b5e71aedf1c7e369f7
Roman Korsun <romakorsun2000@gmail.com>
2024-01-17 13:28:00 +0200

    debug: remove lf_inherited_tags

--- dbt/include/athena/macros/materializations/models/view/view.sql
+++ dbt/include/athena/macros/materializations/models/view/view.sql
@@ -1,10 +1,5 @@
 {% materialization view, adapter='athena' -%}
     {%- set identifier = model['alias'] -%}
-
-    {%- set lf_tags_config = config.get('lf_tags_config') -%}
-    {%- set lf_inherited_tags = config.get('lf_inherited_tags') -%}
-    {%- set lf_grants = config.get('lf_grants') -%}
-
     {%- set versions_to_keep = config.get('versions_to_keep', default=4) -%}
     {%- set target_relation = api.Relation.create(identifier=identifier,
                                                 schema=schema,
@@ -14,14 +9,6 @@
     {% set to_return = create_or_replace_view(run_outside_transaction_hooks=False) %}
 
     {% do adapter.expire_glue_table_versions(target_relation, versions_to_keep, False) %}
-
-    {% if lf_tags_config is not none %}
-      {{ adapter.add_lf_tags(target_relation, lf_tags_config, lf_inherited_tags) }}
-    {% endif %}
-
-    {% if lf_grants is not none %}
-      {{ adapter.apply_lf_grants(target_relation, lf_grants) }}
-    {% endif %}
 
     {% set target_relation = this.incorporate(type='view') %}
 
