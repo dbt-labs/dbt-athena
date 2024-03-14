2234f5389651ff1d436cbb1c193f6a5b89a61957
nicor88 <6278547+nicor88@users.noreply.github.com>
2024-03-14 10:30:04 +0100

    add tests

---
+++ tests/functional/adapter/test_ha_iceberg.py
@@ -0,0 +1,70 @@
+import pytest
+
+from dbt.contracts.results import RunStatus
+from dbt.tests.util import run_dbt
+
+models__table_iceberg_naming_table_unique = """
+{{ config(
+        materialized='table',
+        table_type='iceberg',
+        s3_data_naming='table_unique'
+    )
+}}
+
+select
+    1 as id,
+    'test 1' as name
+union all
+select
+    2 as id,
+    'test 2' as name
+"""
+
+models__table_iceberg_naming_table = """
+{{ config(
+        materialized='table',
+        table_type='iceberg',
+        s3_data_naming='table'
+    )
+}}
+
+select
+    1 as id,
+    'test 1' as name
+union all
+select
+    2 as id,
+    'test 2' as name
+"""
+
+
+class TestTableIcebergTableUnique:
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {"table_iceberg_table_unique.sql": models__table_iceberg_naming_table_unique}
+
+    def test__table_creation(self, project):
+        relation_name = "table_iceberg_table_unique"
+        model_run_result_row_count_query = f"select count(*) as records from {project.test_schema}.{relation_name}"
+
+        model_run = run_dbt(["run", "--select", relation_name])
+        model_run_result = model_run.results[0]
+        assert model_run_result.status == RunStatus.Success
+
+        models_records_count = project.run_sql(model_run_result_row_count_query, fetch="all")[0][0]
+
+        assert models_records_count == 2
+
+
+# in case s3_data_naming=table for iceberg a compile error must be raised
+# with this test we want to be sure that this type of behavior is not violated
+class TestTableIcebergTable:
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {"table_iceberg_table.sql": models__table_iceberg_naming_table}
+
+    def test__table_creation(self, project):
+        relation_name = "table_iceberg_table"
+
+        with pytest.raises(Exception):
+            run_dbt(["run", "--select", relation_name])
