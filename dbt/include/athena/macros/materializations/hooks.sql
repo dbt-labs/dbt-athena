b812938d3a5f2a37444f1c3aeeec03074ddd1801
CommonCrisis <44769652+CommonCrisis@users.noreply.github.com>
2024-04-15 14:29:30 +0200

    Update hooks.sql
    optimize\W+\w+\W+rewrite data using bin_pack does not match
    
    optimize awsdatacatalog.db_name.fct_table_name__inc rewrite data using bin_pack

--- dbt/include/athena/macros/materializations/hooks.sql
+++ dbt/include/athena/macros/materializations/hooks.sql
@@ -3,7 +3,7 @@
   {% for hook in hooks | selectattr('transaction', 'equalto', inside_transaction) %}
     {% set rendered = render(hook.get('sql')) | trim %}
     {% if (rendered | length) > 0 %}
-      {%- if re.match("optimize\W+\w+\W+rewrite data using bin_pack", rendered.lower(), re.MULTILINE) -%}
+      {%- if re.match("optimize\W+\S+\W+rewrite data using bin_pack", rendered.lower(), re.MULTILINE) -%}
         {%- do adapter.run_optimize_with_partition_limit_catching(rendered) -%}
       {%- else -%}
         {% call statement(auto_begin=inside_transaction) %}
