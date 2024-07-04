6d99b607dad30830e0bde9da14445435fc17f970
Theo Tolv <theo@iconara.net>
2024-07-04 14:15:17 +0200

    chore: add tests for all on_schema_change modes
    The previous commit included tests for the sync_all_columns mode, since that had a bug. There are no functional tests that cover the on_schema_change modes, so this adds coverage for the other three modes.
    
    I also added an additional check to ensure the column names have actually been changed (or not changed).

--- tests/functional/adapter/test_hive_on_schema_change.py
+++ tests/functional/adapter/test_hive_on_schema_change.py
@@ -1,7 +1,7 @@
 import pytest
 
 from dbt.contracts.results import RunStatus
-from dbt.tests.util import run_dbt
+from dbt.tests.util import run_dbt, run_dbt_and_capture
 
 models__table_base_model = """
 {{
@@ -25,10 +25,20 @@ select
 class TestHiveOnSchemaChange:
     @pytest.fixture(scope="class")
     def models(self):
-        return {"hive_on_schema_change.sql": models__table_base_model}
+        return {
+            "hive_on_schema_change_sync_all_columns.sql": models__table_base_model,
+            "hive_on_schema_change_append_new_columns.sql": models__table_base_model,
+            "hive_on_schema_change_ignore.sql": models__table_base_model,
+            "hive_on_schema_change_fail.sql": models__table_base_model,
+        }
+
+    def _column_names(self, project, relation_name):
+        result = project.run_sql(f"show columns from {relation_name}", fetch="all")
+        column_names = [row[0].strip() for row in result]
+        return column_names
 
     def test__sync_all_columns(self, project):
-        relation_name = "hive_on_schema_change"
+        relation_name = "hive_on_schema_change_sync_all_columns"
         args = ["run", "--select", relation_name, "--vars", '{"on_schema_change_strategy": "sync_all_columns"}']
 
         model_run_initial = run_dbt(args)
@@ -36,3 +46,46 @@ class TestHiveOnSchemaChange:
 
         model_run_incremental = run_dbt(args)
         assert model_run_incremental.results[0].status == RunStatus.Success
+
+        new_column_names = self._column_names(project, relation_name)
+        assert new_column_names == ["id", "name", "updated_at"]
+
+    def test__append_new_columns(self, project):
+        relation_name = "hive_on_schema_change_append_new_columns"
+        args = ["run", "--select", relation_name, "--vars", '{"on_schema_change_strategy": "append_new_columns"}']
+
+        model_run_initial = run_dbt(args)
+        assert model_run_initial.results[0].status == RunStatus.Success
+
+        model_run_incremental = run_dbt(args)
+        assert model_run_incremental.results[0].status == RunStatus.Success
+
+        new_column_names = self._column_names(project, relation_name)
+        assert new_column_names == ["id", "name", "updated_at"]
+
+    def test__ignore(self, project):
+        relation_name = "hive_on_schema_change_ignore"
+        args = ["run", "--select", relation_name, "--vars", '{"on_schema_change_strategy": "ignore"}']
+
+        model_run_initial = run_dbt(args)
+        assert model_run_initial.results[0].status == RunStatus.Success
+
+        model_run_incremental = run_dbt(args)
+        assert model_run_incremental.results[0].status == RunStatus.Success
+
+        new_column_names = self._column_names(project, relation_name)
+        assert new_column_names == ["id", "name"]
+
+    def test__fail(self, project):
+        relation_name = "hive_on_schema_change_fail"
+        args = ["run", "--select", relation_name, "--vars", '{"on_schema_change_strategy": "fail"}']
+
+        model_run_initial = run_dbt(args)
+        assert model_run_initial.results[0].status == RunStatus.Success
+
+        model_run_incremental, log = run_dbt_and_capture(args, expect_pass=False)
+        assert model_run_incremental.results[0].status == RunStatus.Error
+        assert "The source and target schemas on this incremental model are out of sync!" in log
+
+        new_column_names = self._column_names(project, relation_name)
+        assert new_column_names == ["id", "name"]
