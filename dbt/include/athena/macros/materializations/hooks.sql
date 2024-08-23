9b794449b9f97a6aa6e6a2d110f7e65fe13c71a6
Jérémy Guiselin <9251353+Jrmyy@users.noreply.github.com>
2024-08-23 09:37:48 +0200

    fix: vacuum more runs needed error (#703)

--- dbt/include/athena/macros/materializations/hooks.sql
+++ dbt/include/athena/macros/materializations/hooks.sql
@@ -4,7 +4,9 @@
     {% set rendered = render(hook.get('sql')) | trim %}
     {% if (rendered | length) > 0 %}
       {%- if re.match("optimize\W+\S+\W+rewrite data using bin_pack", rendered.lower(), re.MULTILINE) -%}
-        {%- do adapter.run_optimize_with_partition_limit_catching(rendered) -%}
+        {%- do adapter.run_operation_with_potential_multiple_runs(rendered, "optimize") -%}
+      {%- elif re.match("vacuum\W+\S+", rendered.lower(), re.MULTILINE) -%}
+        {%- do adapter.run_operation_with_potential_multiple_runs(rendered, "vacuum") -%}
       {%- else -%}
         {% call statement(auto_begin=inside_transaction) %}
           {{ rendered }}
