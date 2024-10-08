67a11fac3715ccb842e6ab9fee662ed3eadb7dfc
Mike Alfare <mike.alfare@dbtlabs.com>
2024-10-08 19:46:52 -0400

    update plugin name

--- dbt-athena/tests/unit/test_config.py
+++ dbt-athena/tests/unit/test_config.py
@@ -12,7 +12,7 @@ class TestConfig:
         num_boto3_retries = 5
         get_boto3_config.cache_clear()
         config = get_boto3_config(num_retries=num_boto3_retries)
-        assert config._user_provided_options["user_agent_extra"] == "dbt-athena-community/2.4.6"
+        assert config._user_provided_options["user_agent_extra"] == "dbt-athena/2.4.6"
         assert config.retries == {"max_attempts": num_boto3_retries, "mode": "standard"}
 
 
