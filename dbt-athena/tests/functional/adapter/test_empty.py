8f6b1f0813500677bdc81e0b524df0b4c7db1496
David Lequin <david.lequin@gmail.com>
2024-12-02 17:35:37 +0000

    Stop adding aliases to render_limited output (#749)

--- dbt-athena/tests/functional/adapter/test_empty.py
+++ dbt-athena/tests/functional/adapter/test_empty.py
@@ -1,5 +1,9 @@
-from dbt.tests.adapter.empty.test_empty import BaseTestEmpty
+from dbt.tests.adapter.empty.test_empty import BaseTestEmpty, BaseTestEmptyInlineSourceRef
 
 
 class TestAthenaEmpty(BaseTestEmpty):
+    pass
+
+
+class TestAthenaEmptyInlineSourceRef(BaseTestEmptyInlineSourceRef):
     pass
